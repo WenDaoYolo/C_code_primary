4c203c5fb0a0435a14a6be45653e72232e4b87c7
WenDaoYolo <3547474131@qq.com>
2025-02-19 14:47:33 +0800

    Update character.cpp

--- character.cpp
+++ character.cpp
@@ -2,20 +2,20 @@
 #include<stdio.h>
 #include<string.h>
 int main() {
-	 //ת���ַ���ͨ��\���,ת����ԭ������˼���������⺬��
-	printf("ab\ncd\n");//����
-	printf("C:\test\team\n");//ˮƽ�Ʊ�
-	printf("\a");//������ʾ-����
-	//��ת���ַ������������ַ����,\+���������ַ�
-	//ת���ַ��ǵ����ַ���Ҳռһ���ֽڵĿռ�
+	 //转义字符，通过\组合,转变了原本的意思，有着特殊含义
+	printf("ab\ncd\n");//换行
+	printf("C:\test\team\n");//水平制表
+	printf("\a");//警告提示-蜂鸣
+	//反转义字符，用于特殊字符输出,\+单个特殊字符
+	//转义字符是单个字符，也占一个字节的空间
 	printf("%s", "\"");
 	printf("%c", '\'');
 	printf("%c", '\\');
-	//other:\b,\r,\f,\v,\ddd(8����),\xdd(16����)
-	//ÿ���ַ����ж�Ӧ��ʮ����ASCLL�룬
-	//���ַ���������ʽ���ʱ�������Ӧ��ASCLL�룬��֮��Ȼ
+	//other:\b,\r,\f,\v,\ddd(8进制),\xdd(16进制)，\0空字符
+	//每个字符都有对应的十进制ASCLL码，
+	//当字符以整数形式输出时会输出对应的ASCLL码，反之亦然
 	printf("%d", 'a');
 	printf("%c",65);
-	//���ASCLL����ձ�
+	//详见ASCLL码对照表
 	return 0;
 }
