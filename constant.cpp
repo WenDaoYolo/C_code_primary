9cad21380490894f4be0bd5a47008431b8e617a3
WenDaoYolo <3547474131@qq.com>
2025-02-22 17:09:43 +0800

    Update constant.cpp

--- constant.cpp
+++ constant.cpp
@@ -12,10 +12,11 @@ int main() {
 		printf("%d\n",arr[n]);
 	}
 	printf("#define定义的标识符常量MAX=%d\n",MAX);
-	enum Sex { MALE,FEMALE=9 };//自定义的枚举常量数据类型Sex---一组常量的集合形成的一个枚举类型，类似于集合
-	enum Sex t = MALE;		//定义enum Sex类型的常量ts
+	//枚举常量:定义一组常量的集合形成的一个枚举类型，若未赋初值，默认从第一个元素开始为0，往后依次增加
+	enum Sex { MALE,FEMALE=9 };//自定义的枚举常量数据类型Sex
+	enum Sex t = MALE;		//定义Sex类型的常量t
 	enum Sex m = FEMALE;
-	printf("我是自定义的枚举常量数据类型MALE我未赋初值，默认值为：%d\n", t);//枚举常量从第一个元素开始，若未赋初值，其数值默认固定为0，往后依次增加
+	printf("我是自定义的枚举常量数据类型MALE我未赋初值，默认值为：%d\n", t);//
 	printf("我是自定义的枚举常量数据类型FEMALE,我赋了初值：%d", m);
 	return 0;
 }
