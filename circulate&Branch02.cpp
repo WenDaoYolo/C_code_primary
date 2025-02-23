e585a7472e5465c6e9c29a52eba67a12f3e93bdc
WenDaoYolo <3547474131@qq.com>
2025-02-23 19:58:43 +0800

    Update and rename structure2.cpp to circulate&Branch02.cpp

---
+++ circulate&Branch02.cpp
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+int main() {
+	int x = 0;
+	int n = 3;
+	while (1) {
+		printf("我是while循环\n");
+		if (x > 2)
+			break;//break跳出整个循环(只能跳出当前所在层的循环,多层循环嵌套需要分别用多次break)
+				  //continue跳出本次循环(只结束本次循环,不会跳出整个循环)
+		x++;
+	}
+	do {
+		printf("我是do while循环\n");
+	}while(n<3);
+	if (n == 2)
+		printf("我是选择语句1\n");
+	else if (n == 3)
+		printf("我是选择语句2\n");
+	else
+		printf("我是选择语句3\n");
+	
+	
+	return 0;
+}
