9e201a3a69c4664a1d3e45515231264bb6f4546d
WenDaoYolo <3547474131@qq.com>
2025-02-26 00:19:58 +0800

    Add files via upload

--- structure.cpp
+++ structure.cpp
@@ -4,8 +4,8 @@
 	结构体structure
 	用来描述和创造更复杂的数据类型,比如学生这个类型就包含名字，学号，班级等，而这些成员数据可能都是不同的类型
 	//多个不同类型的数据构成的复合数据类型
-	 tip:创建的结构体的类型，应为本质是属于结构体大类的，所以其完整类型为大类下的自定义类型：struct 结构体名
-	struct 结构名{
+	 tip:定义的结构体本质上是一个类类型(struct大类下的自定义类),在使用定义的结构体类型时，前面的struct可以省去(默认会自动添加)
+		结构名{
 		类型 成员1；
 		类型 成员2；
 		...
@@ -13,14 +13,15 @@
 */
 //创建一个Stu的数据类型
 int main() {
-	struct Stu{
+	struct Stu {
 		char name[20];
 		int age;
 		char Sex[20];
-	};								
-	Stu stu1 = { "小王",20,"男" };	//创建一个Stu类型的变量stu1,并为其赋初值(赋初值又称为<初始化>)
-	Stu stu2 = { "小红",21,"女" };	//创建一个Stu类型的变量stu2,并为其赋初值
-									//实现创建了各种不同的学生
+	};
+	 Stu stu1 = { "小王",20,"男升" };	//创建一个Stu类型的变量stu1,并为其赋初值(赋初值又称为<初始化>)
+	struct Stu stu2 = { "小红",21,"女" };	//创建一个Stu类型的变量stu2,并为其赋初值
+	//实现创建了各种不同的学生
+
 
 //创建一个Book的数据类型
 	struct Book{
@@ -33,9 +34,21 @@ int main() {
 	                                                  //实现创建了各种不同的书籍
 	//.结构体调用操作符,-> 
 	//访问调用结构体复合类型中的具体类型(即结构体中的成员):结构体名.成员名			
-	printf("1:%s性别%s,年龄%d\n",stu1.name,stu1.Sex,stu1.age);	//调用结构体中的成员并输出
-	struct Stu * ps = &stu1;										//将结构体的地址赋值给指针变量p
+	printf("1:%s性别%s,年龄%d\n",stu2.name,stu2.Sex,stu2.age);	//调用结构体中的成员并输出
+	
+	
+	Stu * p= &stu1;										//将结构体的地址赋值给指针变量p
+	printf("2:%s性别%s,年龄%d\n", (*p).name, (*p).Sex, (*p).age);	//????
+	
+	struct stuu {
+		double score;
+		char id[20];
+		char name[20];
+	};
+	stuu s = { 33.2,"2011","wangwu" };
+	struct stuu* p1 = &s;
+	printf("%s,%lf,%s\n", (*p1).id, (*p1).score, (*p1).name);
 
-	printf("2:%s性别%s,年龄%d\n",*(ps).name, *p.Sex, *p.age);	//????
+
 	return 0;
 }
