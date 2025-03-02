969fadd1abfe85d48c56f4f67f749ede9354cde1
WenDaoYolo <3547474131@qq.com>
2025-03-02 22:07:18 +0800

    Update circulate&Branch02.cpp

--- circulate&Branch02.cpp
+++ circulate&Branch02.cpp
@@ -8,7 +8,7 @@ int main() {
 		printf("我是while循环\n");
 		if (x > 2)
 			break;//break跳出当前循环/结构(只能跳出当前所在层的循环,多层循环嵌套需要分别用多次break)
-				  //continue跳出本次循环(只结束本次循环,不会跳出当前整个循环)
+				  //continue跳出本次循环(<跳过conitue后面的代码>,继续下一次循环)
 		x++;
 	}
 	do {
