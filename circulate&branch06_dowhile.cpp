59fcd078ea3ede97b30e1f6097f342eb7b2d5270
WenDaoYolo <3547474131@qq.com>
2025-03-05 22:38:30 +0800

    Update circulate&branch06_dowhile.cpp

--- circulate&branch06_dowhile.cpp
+++ circulate&branch06_dowhile.cpp
@@ -2,47 +2,47 @@
 #include<stdio.h>
 int main(){
 	/*
-		do while��whileѭ��һ��,Ψһ������do while���ٻ�ִ��һ��
+		do while和while循环一致,唯一区别是do while至少会执行一次
 		
 		do{
-			ѭ�����
-			...			�����	
-		}while(����);		
+			循环语句
+			...			代码块	
+		}while(条件);		
 	
 	*/
-	//����ѭ������ϰ
+	//三种循环的练习
 	int n,i=1,value1 = 1;
-	printf("������n:\n");
+	printf("请输入n:\n");
 	scanf("%d",&n);
 	while (i <= n) {
 		value1 = (value1) * i;
 		i++;
 	}
-	printf("%d�Ľײ���:%d\n",n,value1);
+	printf("%d的阶层是:%d\n",n,value1);
 		
 	int sum = 0,value2=1;
 	for (int j = 1; j <= 10; j++) {
 		for (int k = 1; k <= j; k++)
-			value2 *= k;			//�ײ�ļ���
-				sum += value2;		//�ײ������ۼ�
-					value2 = 1;		//��ʼ���ײ����value2
+			value2 *= k;			//阶层的计算
+				sum += value2;		//阶层结果的累加
+					value2 = 1;		//初始化阶层变量value2
 	}
-	printf("1��10�ײ�ĺ�Ϊ:%d\n",sum);
+	printf("1到10阶层的和为:%d\n",sum);
 
 	/*
-		�㷨�Ż�:������ѧ˼��
+		算法优化:本质数学思想
 		3!=3*2*1
 		2!=2*1
 		1!=1
-		����3!=3*2!
+		所以3!=3*2!
 			2!=2*1!
 			1!=1
 	*/
 	int test = 1,test2=0;
-	for (int m = 1; m <=10; m++) {	//ͨ����ǰ�ײ�����һ���ײ�Ľ���������ٵ�������ѭ������
-		test *= m;		//�Ż�����һ��ѭ��	�����ü���ײ����������ʹ������������һ���ײ�
+	for (int m = 1; m <=10; m++) {	//通过当前阶层求下一个阶层的结果，而非再单独重新循环计算
+		test *= m;		//优化掉了一层循环	不重置计算阶层变量，继续使用这个结果求下一个阶层
 		test2 += test;
 	}
-	printf("�㷨2:%d\n",test2);
+	printf("算法2:%d\n",test2);
 	return 0;
 }
